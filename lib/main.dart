dc534c000290337a6579594d12658edcd94cc129
Eldot <laurence.campbell@gmail.com>
2021-02-15 18:08:40 +0000

    Replaced Provider with a ChangeNotifierProvider to use a MyModel class to allow the UI to display changes to the MyModel data

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,14 @@ import 'package:provider/provider.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String data = 'Top Secret Data';
-
   @override
   Widget build(BuildContext context) {
-    return Provider<String>(
-      create: (context) => data,
+    return ChangeNotifierProvider<MyModel>(
+      create: (context) => MyModel(),
       child: MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text(data),
+            title: MyAppTitleText(),
           ),
           body: Level1(),
         ),
@@ -34,8 +32,11 @@ class Level1 extends StatelessWidget {
 class Level2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Level3(),
+    return Column(
+      children: [
+        MyTextField(),
+        Level3(),
+      ],
     );
   }
 }
@@ -43,6 +44,32 @@ class Level2 extends StatelessWidget {
 class Level3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(Provider.of<String>(context));
+    return Text(Provider.of<MyModel>(context).message);
+  }
+}
+
+class MyAppTitleText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(Provider.of<MyModel>(context, listen: false).message); // Only show at start
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (newText) {
+        Provider.of<MyModel>(context).setMessage(newText);
+      },
+    );
+  }
+}
+
+class MyModel extends ChangeNotifier {
+  String message = 'Top Secret Data';
+  void setMessage(String newText) {
+    message = newText;
+    notifyListeners();
   }
 }
